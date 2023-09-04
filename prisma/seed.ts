8d1794c77a257394a3034ec909e51ebb2433723e
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2023-09-04 22:44:53 +0500

    [FEAT] Added basic seeder :muscle:

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -10,7 +10,32 @@ async function seed() {
 
   const { data } = await axios.get('list_movies.json?quality=3D&limit=50');
 
-  console.log('response', data);
+  const response = JSON.parse(data);
+
+  for (const movie of response.data.movies) {
+    const createdMovie = await prisma.cinematic.create({
+      data: {
+        referenceId: movie.id,
+        title: movie.title,
+        releasedYear: movie.year,
+        description: movie.description_full,
+        imdb: movie.imdb_code,
+        language: movie.language,
+        poster: movie.large_cover_image,
+        slug: movie.slug,
+        runtime: movie.runtime,
+        posterHash: '',
+        youtubeTrailerCode: movie.yt_trailer_code,
+        pictures: [],
+        genres: [],
+        covers: [],
+      },
+    });
+
+    console.log('createdMovie', createdMovie);
+  }
+
+  console.log('Inserted successfully!!!');
 }
 
 seed();
